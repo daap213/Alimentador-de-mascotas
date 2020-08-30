8513b1b55f319b25245d0c0c75538f272e4f69d4
tamikgr <tkgarcia@espol.edu.ec>
2020-08-30 03:00:58 -0500

    correccion del spinner

--- app/src/main/java/com/example/proyecto/RegistroMascota.java
+++ app/src/main/java/com/example/proyecto/RegistroMascota.java
@@ -19,13 +19,13 @@ import com.example.proyecto.entidades.Usuario;
 import com.example.proyecto.utilidades.Utilidades;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RegistroMascota extends AppCompatActivity {
     EditText razaMascota,nombreMascota, tamaño, edadMascota;
-    Spinner spinnerDueño;
-
-    ArrayList<String> listaUsuarios;
-    ArrayList<Usuario> usuariosList;
+    Spinner spinnerTamaño;
+    List tamañosList = new ArrayList( );
+   // ArrayList<String> listaUsuarios;
 
     AdminSQLiteOpenHelper admin;
 
@@ -36,20 +36,23 @@ public class RegistroMascota extends AppCompatActivity {
 
         razaMascota = (EditText) findViewById(R.id.raza);
         nombreMascota = (EditText) findViewById(R.id.nombreMascota);
-        tamaño = (EditText) findViewById(R.id.tamaño);
         edadMascota = (EditText) findViewById(R.id.edadMascota);
-        spinnerDueño= (Spinner) findViewById(R.id.spinnerdueño);//para escoger el dueño q corresponde a la mascota
+        spinnerTamaño= (Spinner) findViewById(R.id.spinnerTamaño);//para escoger el tamaño de la mascota
 
         admin=new AdminSQLiteOpenHelper(getApplicationContext(),"PST_G6",null,1);
 
-        consultarListaUsuarios();
+        tamañosList.add("Pequeño");
+        tamañosList.add("Mediano");
+        tamañosList.add("Grande");
+
+        //consultarListaUsuarios();
 
         ArrayAdapter<CharSequence> adaptador=new ArrayAdapter
-                (this,android.R.layout.simple_spinner_item,listaUsuarios);
+                (this,android.R.layout.simple_spinner_item,tamañosList);
 
-        spinnerDueño.setAdapter(adaptador);
+        spinnerTamaño.setAdapter(adaptador);
 
-        spinnerDueño.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinnerTamaño.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long idl) {
             }
@@ -71,32 +74,33 @@ public class RegistroMascota extends AppCompatActivity {
         values.put(Utilidades.KEY_TAMAÑO,tamaño.getText().toString());
         values.put(Utilidades.KEY_MASCOTA_EDAD,edadMascota.getText().toString());
 
-        int idCombo= (int) spinnerDueño.getSelectedItemId();
+        int idCombo= (int)spinnerTamaño.getSelectedItemId();
         /**
-         * Valida la seleccion del combo de dueños, si el usuario elige "seleccione" entonces
+         * Valida la seleccion del combo de los tamaños, si el usuario elige "seleccione" entonces
          * se retorna el id 0 ya que la palabra "seleccione" se encuentra en la pos 0 del combo,
          * sinó entonces se retorna la posicion del combo para consultar el usuario almacenado en la lista
          */
         if (idCombo!=0){
-            Log.i("TAMAÑO",usuariosList.size()+"");
+            Log.i("TAMAÑO",tamañosList.size()+"");
             Log.i("id combo",idCombo+"");
             Log.i("id combo - 1",(idCombo-1)+"");//se resta 1 ya que se quiere obtener la posicion de la lista, no del combo
-            int idDuenio=usuariosList.get(idCombo-1).getId();
-            Log.i("id DUEÑO",idDuenio+"");
 
-            values.put(Utilidades.KEY_MASCOTA_ID_USUARIO,idDuenio);
+          //  int tamaño=tamañosList.get(idCombo-1).;
+          //  Log.i("id DUEÑO",idDuenio+"");
 
-            Long idResultante=db.insert(Utilidades.TABLA_MASCOTA,Utilidades.KEY_MASCOTA_ID,values);
-            Toast.makeText(getApplicationContext(),"Id Registro: "+idResultante,Toast.LENGTH_SHORT).show();
+         //   values.put(Utilidades.KEY_TAMAÑO,tamaño);
+
+            Long tamañoEscogido=db.insert(Utilidades.TABLA_MASCOTA,Utilidades.KEY_TAMAÑO,values);
+            Toast.makeText(getApplicationContext(),"Tamaño: "+tamañoEscogido,Toast.LENGTH_SHORT).show();
             db.close();
             limpiar();
 
         }else{
-            Toast.makeText(getApplicationContext(),"Debe seleccionar un Dueño",Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(),"Debe seleccionar un Tamaño",Toast.LENGTH_LONG).show();
         }
     }
 
-    private void consultarListaUsuarios() {
+   /* private void consultarListaUsuarios() {
         SQLiteDatabase db=admin.getReadableDatabase();
 
         Usuario persona=null;
@@ -132,7 +136,7 @@ public class RegistroMascota extends AppCompatActivity {
         for(int i=0;i<usuariosList.size();i++){
             listaUsuarios.add(usuariosList.get(i).getId()+" - "+usuariosList.get(i).getNombreUsuario());
         }
-    }
+    }*/
 
     public void onClick(View view) {
         switch (view.getId()){
