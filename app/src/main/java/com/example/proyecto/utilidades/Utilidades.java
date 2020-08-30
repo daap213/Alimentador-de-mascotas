ba09c511ebb50d29932d802e5c3b46f99b52c795
tamikgr <tkgarcia@espol.edu.ec>
2020-08-29 20:03:48 -0500

    creacion de la TABLA HORARIO

--- app/src/main/java/com/example/proyecto/utilidades/Utilidades.java
+++ app/src/main/java/com/example/proyecto/utilidades/Utilidades.java
@@ -24,6 +24,13 @@ public class Utilidades {
     public static final String KEY_MASCOTA_EDAD= "edadMascota";
     public static final String KEY_TAMAÑO="tamaño";
 
+    //COLUMNAS DE LA TABLA DE HORARIO
+    public static final String KEY_HORARIO_HORA= "hora";
+    public static final String KEY_HORARIO_DESCRIPCION= "descripcion";
+    public static final String KEY_HORARIO_IDUSUARIO= "idUsuario";
+    public static final String KEY_HORARIO_TONO="tono";
+
+
     //CREACION DE LAS TABLAS
     public static final String CREAR_TABLA_USUARIO="CREATE TABLE " +
             ""+TABLA_USUARIO+" ("+KEY_USUARIO_ID + " "+
@@ -41,4 +48,10 @@ public class Utilidades {
             KEY_MASCOTA_EDAD + " VARCHAR," +
             KEY_RAZA + " VARCHAR," +
             KEY_TAMAÑO+ " INTEGER)";
+
+    public static final String CREAR_TABLA_HORARIO="CREATE TABLE " +
+            ""+TABLA_HORARIO+" ("+KEY_HORARIO_HORA+ " DATE,"+
+            KEY_HORARIO_DESCRIPCION + " VARCHAR," +
+            KEY_HORARIO_IDUSUARIO+ " INTEGER REFERENCES "+ TABLA_USUARIO+","+
+            KEY_HORARIO_TONO+ " VARCHAR)";//NO SE SI ESTE ESTE BIEN, EL VARCHAR
 }
